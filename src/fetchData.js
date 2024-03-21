90043d923d017ea5a5463b06c7301b0b9a741442
Scott Davis <scott@goodjob.io>
2024-03-20 18:18:23 -0600

    add delete button, clean up styles

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,32 +1,26 @@
 export function fetchData() {
-  const container = document.getElementById("data-container");
+  const container = document.getElementById('data-container');
 
-  fetch("attendanceData.json")
+  fetch('attendanceData.json')
     .then((response) => response.json())
     .then((data) => {
       data.forEach((entry) => {
         console.log(entry);
-        const entryElement = document.createElement("div");
-        entryElement.classList.add(
-          "bg-white",
-          "rounded-lg",
-          "p-3",
-          "mb-4",
-          "max-w-[400px]",
-          "min-w-60",
-          "grow",
-          "shadow-md"
-        );
+        const entryElement = document.createElement('div');
+        entryElement.classList.add('bg-white', 'rounded-lg', 'p-3', 'mb-4', 'max-w-[400px]', 'min-w-60', 'grow', 'shadow-md', 'relative');
         entryElement.innerHTML = `
           <p class="font-medium">${entry.userName}</p>
           <p>${entry.locationName}</p>
           <p>${entry.locationAddress}</p>
           <p>${entry.floorName}</p>
-          <p>${entry.seatName}</p>
+            <p>${entry.seatName}</p>
+            <button class="bg-accent w-32 py-1 rounded-md mt-6 text-white absolute bottom-2 right-2 hover:bg-secondary transition-all ease-in-out duration-200">
+              Delete
+            </button>
         `;
 
         container.appendChild(entryElement);
       });
     })
-    .catch((error) => console.error("Failed to load JSON data: :", error));
+    .catch((error) => console.error('Failed to load JSON data: :', error));
 }
